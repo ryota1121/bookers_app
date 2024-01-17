accdaa23484c5e29552e80786113241185f3f0f7
EC2 Default User <ec2-user@ip-172-31-1-155.ap-northeast-1.compute.internal>
2024-01-17 10:47:46 +0000

    1/17 -2 commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "An error has occurred"
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])  
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "An error has occurred"
+      render :new
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "9d4e628d12a84806bb939b0c94effe44.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
